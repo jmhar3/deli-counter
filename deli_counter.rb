d9e5b9232295ad2c2271fe4ffdc41d177c170c5c
jmhar <jmhar@protonmail.com>
2021-09-01 13:42:34 +1000

    customer management

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+katz_deli = []
+
+def line queue
+    if queue.length == 0
+        puts "The line is currently empty."
+    else
+        current_queue = queue.map.with_index(1) { |customer, i| "#{i}. #{customer}" }.join(" ")
+        puts "The line is currently: #{current_queue}"
+    end
+end
+
+def take_a_number current_queue, new_customer
+    current_queue << new_customer
+    puts "Welcome, #{new_customer}. You are number #{current_queue.length} in line."
+end
+
+def now_serving queue
+    if queue.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{queue[0]}."
+        queue.shift
+    end
+end
